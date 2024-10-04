77404258307b192625a38082ce46da9db62337cd
PeterMwitiDev <mwitipet@gmal.com>
2024-10-04 23:46:50 +0300

    Product and auth routes

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+
+import authRoutes from "./routes/auth.route.js"
+import productRoutes from "./routes/product.route.js"
+
+import { connectDB } from "./lib/db.js"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000 
+
+app.use(express.json())//allow you to parse
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/products", productRoutes)
+
+app.listen(PORT, () => {
+    console.log("server is running on http://localhost:" + PORT)
+
+    connectDB()
+})
